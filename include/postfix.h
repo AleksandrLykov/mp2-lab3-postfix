5ec841173c1a75f0de7e6dc47ffa37e83e523213
AleksandrLykov <aleksandrlykov0@gmail.com>
2015-12-25 11:20:37 +0300

    test2
    начало 2

--- include/postfix.h
+++ include/postfix.h
@@ -1,4 +1,28 @@
 #ifndef __POSTFIX_H__
 #define __POSTFIX_H__
 
+#include "list_stack.h"
+#include <string>
+
+using namespace std;
+
+class Postfix {
+private:
+	stack<char>* res;
+	stack<char>* oper;	
+	string v1; // исходное выражение
+	string v2; // готовое выражение
+	int kolvo; // количество операций
+public:
+	Postfix();
+	~Postfix();
+	
+	int prioritet(const char);
+	int operand (const char);
+	int operaci (const char); //приоритет операции
+	char skobka (); // если попалась скобка
+	char priorit (char b); // если приоритет новой операции меньше старого
+}
+
+
 #endif
--- postfix/postfix.cpp
+++ postfix/postfix.cpp
@@ -1,11 +1,57 @@
 #include "postfix.h"
 
-#include <iostream>
-
-using namespace std;
-
-void main () 
-{
-	cout << 1;
-	system ("pause");
+Postfix::Postfix() {
+	oper = new stack<char>();
+	res = new stack<char>();
+	v1 = "";
+	v2 = "";
+	kolvo = 0;
 }
+
+Postfix::~Postfix(void) {
+	delete res;
+	delete oper;
+}
+
+int Postfix::operaci(const char a) 
+{
+	switch (a)
+	{
+	case '*':
+		return 3;
+	case '/':
+		return 3;
+	case '+':
+		return 2;
+	case '-':
+		return 2;
+	case '(':
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+int Postfix::operand(const char a) 
+{
+	if (a == ' ') 
+		return 0;
+	return 1;
+}
+
+char Postfix::skobka()
+{
+	char a = oper->pop();
+	while (a != '(')
+	{
+		res->push(a);
+		a = oper->pop();
+	}
+	a = oper->pop();
+	return (a);
+}
+
+char Postfix::priorit(char a) 
+{
+	char b = oper->pop();
+	while ((a
